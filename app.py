2aa9886c4787fa8ec7d3b4d943237a8a5c503402
Bruno C. Mundim <mundim@th.physik.uni-frankfurt.de>
2016-09-02 17:33:39 -0400

    Introduce logic to get the Quandl API key.

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
+import os
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return redirect('/index')
+   return redirect('/index')
 
 @app.route('/index')
 def index():
-  return render_template('index.html')
+   try:
+      API_KEY = os.environ['QUANDL_API_KEY']
+   except KeyError:
+      print "Not possible to load the environment variable 'QUANDL_API_KEY'"
+      print 'Set API_KEY="" '
+      API_KEY = ""
+  
+   return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+   app.run(port=33507)
