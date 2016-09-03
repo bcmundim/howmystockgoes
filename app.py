6d908f820b36aeee03656dc6456716eff835c342
Bruno C. Mundim <mundim@th.physik.uni-frankfurt.de>
2016-09-02 20:16:41 -0400

    User input form implemented.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
@@ -7,7 +8,7 @@ app = Flask(__name__)
 def main():
    return redirect('/index')
 
-@app.route('/index')
+@app.route('/index',methods=['GET','POST'])
 def index():
    try:
       API_KEY = os.environ['QUANDL_API_KEY']
@@ -15,8 +16,52 @@ def index():
       print "Not possible to load the environment variable 'QUANDL_API_KEY'"
       print 'Set API_KEY="" '
       API_KEY = ""
-  
-   return render_template('index.html')
+
+   # Initialize default criterias:
+   select = [
+   False, # closing price
+   False, # adjusted closing price
+   False, # opening price
+   False  # adjusted opening price
+   ]
+
+   if request.method == 'GET':  
+      return render_template('index.html')
+   else:
+
+      try:
+         ticker = request.form['ticker']
+         if ticker == '':
+            return " Please choose a stock ticker first!"
+      except KeyError:
+         return " Please choose a stock ticker first!"
+
+      try:
+         select[0] = request.form['closing_price'] == 'yes'
+      except KeyError:
+         select[0] = False
+
+      try:
+         select[1] = request.form['adjusted_closing_price'] == 'yes'
+      except KeyError:
+         select[1] = False
+
+      try:
+         select[2] = request.form['opening_price'] == 'yes'
+      except KeyError:
+         select[2] = False
+
+      try:
+         select[3] = request.form['adjusted_opening_price'] == 'yes'
+      except KeyError:
+         select[3] = False
+
+      if np.any(select):
+         return " Congratulations! You selected at least one property!"
+      else:
+         return " Please select at least one desired feature!"
+
 
 if __name__ == '__main__':
    app.run(port=33507)
+
